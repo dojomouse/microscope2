f1f58fed5538d9bdeb16432595afdee23e3a5217
Nick Butcher <butcher.nick@gmail.com>
2013-06-09 20:23:30 +1200

    9-1 Basic error reporting

--- client/views/posts/post_item.js
+++ client/views/posts/post_item.js
@@ -1,10 +1,10 @@
 Template.postItem.helpers({
 	ownPost: function() {
-		return this.userId == Meteor.userID();
+		return this.userId == Meteor.userId();
 	},
-	domain: function() {
-		var a = document.createElement('a');
-		a.href = this.url;
-		return a.hostname;
-	}
+  	domain: function() {
+    	var a = document.createElement('a');
+    	a.href = this.url;
+    	return a.hostname;
+  	}
 });
--- client/views/posts/post_submit.js
+++ client/views/posts/post_submit.js
@@ -1,18 +1,18 @@
 Template.postSubmit.events({
-	'submit form': function(event) {
-		event.preventDefault();
-
-		var post = {
-			url: $(event.target).find('[name=url]').val(),
-			title: $(event.target).find('[name=title]').val(),
-			message: $(event.target).find('[name=message]').val()
-		}
-
-		Meteor.call('post', post, function(error, id) {
-			if(error)
-				return alert(error.reason);
-			Meteor.Router.to('postPage', id);
-		});
-		Meteor.Router.to('postsList');
-	}
+  'submit form': function(event) {
+    event.preventDefault();
+    
+    var post = {
+      url: $(event.target).find('[name=url]').val(),
+      title: $(event.target).find('[name=title]').val(),
+      message: $(event.target).find('[name=message]').val()
+    }
+    
+    Meteor.call('post', post, function(error, id) {
+      if (error)
+        return alert(error.reason);
+        
+      Meteor.Router.to('postPage', id);
+    });
+  }
 });
