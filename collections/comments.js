53db9ddc253d0e5c018bcc70ce456c74b2810fc2
Nick Butcher <butcher.nick@gmail.com>
2013-06-16 22:13:22 +1200

    10-4 Made a simple pub/sub for comments

--- collections/comments.js
+++ collections/comments.js
@@ -2,7 +2,7 @@ Comments = new Meteor.Collection('comments');
 Meteor.methods({
 	comment: function(commentAttributes) {
 		var user = Meteor.user();
-		var post = Posts..findOne(commentAttributes.postId);
+		var post = Posts.findOne(commentAttributes.postId);
 		// ensure the user is logged in
 		if (!user)
 			throw new Meteor.Error(401, "You need to login to make comments");
@@ -10,7 +10,7 @@ Meteor.methods({
 			throw new Meteor.Error(422, "Please write some content");
 		if (!commentAttributes.postId)
 			throw new Meteor.Error(422, "You must comment on a post");
-		comment = _.extend(_.pick(commentAttributes, 'postId', 'body') {
+		comment = _.extend(_.pick(commentAttributes, 'postId', 'body'), {
 			userId: user._id,
 			author: user.username,
 			submitted: new Date().getTime()
