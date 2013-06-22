e8074bdd2aa8c0737fcf5589c2f1642f88b76666
Nick Butcher <butcher.nick@gmail.com>
2013-06-22 16:51:12 +1200

    12-4 Use a single post subscription to ensure that we can always see the right post

--- client/main.js
+++ client/main.js
@@ -1,6 +1,6 @@
 postsHandle = Meteor.subscribeWithPagination('newPosts', 10);
 Deps.autorun(function() {
-	Meteor.subscrube('singlePost', Session.get('currentPostId'));
+	Meteor.subscribe('singlePost', Session.get('currentPostId'));
 	Meteor.subscribe('comments', Session.get('currentPostId'));
 });
 Meteor.subscribe('notifications');
